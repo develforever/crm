f1cb9b08deb0e17868227717d343566a0b643583
Robert Jamróz <rockfade@gmail.com>
2026-01-06 01:01:59 +0100

    html editor custom element

--- assets/crm/modules/cms/HtmlEditor.tsx
+++ assets/crm/modules/cms/HtmlEditor.tsx
@@ -59,8 +59,26 @@ const HtmlEditor = ({
         while (el.firstChild) {
             parent.insertBefore(el.firstChild, el);
         }
-
         parent.removeChild(el);
+    }
+
+    function findClosestWrapper(
+        node: Node,
+        predicate: (el: HTMLElement) => boolean
+    ): HTMLElement | null {
+        let current: Node | null = node;
+
+        while (current) {
+            if (
+                current.nodeType === Node.ELEMENT_NODE &&
+                predicate(current as HTMLElement)
+            ) {
+                return current as HTMLElement;
+            }
+            current = current.parentNode;
+        }
+
+        return null;
     }
 
     function normalize(root: HTMLElement) {
@@ -70,7 +88,8 @@ const HtmlEditor = ({
         root.querySelectorAll('i i').forEach(el => unwrap(el as HTMLElement));
     }
 
-    const wrapSelection = (tag: 'b' | 'i') => () => {
+    const wrapSelection = (tag: 'b' | 'i' | 'div', attrs?: Record<string, string>) => () => {
+
         const sel = window.getSelection();
         if (!sel || sel.rangeCount === 0) return;
 
@@ -78,7 +97,7 @@ const HtmlEditor = ({
         if (range.collapsed) return;
 
         if (isSelectionInTag(tag)) {
-            // USUŃ FORMAT
+
             const nodes: HTMLElement[] = [];
             let node = sel.anchorNode as Node | null;
 
@@ -100,8 +119,68 @@ const HtmlEditor = ({
         const wrapper = document.createElement(tag);
         wrapper.appendChild(range.extractContents());
         range.insertNode(wrapper);
+        sel.removeAllRanges();
+        normalize(contentRef.current!)
+    }
+
+    const toggleCustomDiv = (attrs: {
+        className: string;
+        [key: string]: string;
+    }) => () => {
+        const sel = window.getSelection();
+        if (!sel || sel.rangeCount === 0) {
+            console.log('no selection');
+            return;
+        }
+
+        const range = sel.getRangeAt(0);
+        if (range.collapsed) {
+            console.log('collapsed');
+            //return;
+        }
+
+        const wrapper = findClosestWrapper(
+            sel.anchorNode!,
+            el =>
+                el.tagName === 'DIV' &&
+                el.classList.contains(attrs.className)
+        );
+
+
+        if (wrapper) {
+            unwrap(wrapper);
+            return;
+        }
+
+        const div = document.createElement('div');
+
+        Object.entries(attrs).forEach(([k, v]) => {
+            if (k === 'className') div.className = v;
+            else div.setAttribute(k, v);
+        });
+
+        //div.contentEditable = 'false';
+        div.innerHTML = '&nbsp;';
+
+        if (range.collapsed) {
+            //div.appendChild(range.commonAncestorContainer);
+            range.insertNode(div);
+        } else {
+
+            div.appendChild(range.extractContents());
+            range.insertNode(div);
+        }
+
+        // const p = document.createElement('p');
+        // p.innerHTML = '&nbsp;';
+        // div.after(p);
+
+        // const newRange = document.createRange();
+        // newRange.setStart(p, 0);
+        // newRange.collapse(true);
 
         sel.removeAllRanges();
+        // sel.addRange(newRange);
         normalize(contentRef.current!)
     }
 
@@ -113,6 +192,10 @@ const HtmlEditor = ({
                 <ButtonGroup>
                     <Button type="button" onClick={wrapSelection('b')}>Bold</Button>
                     <Button type="button" onClick={wrapSelection('i')}>Italic</Button>
+                    <Button type="button" onClick={toggleCustomDiv({
+                        className: 'placeholder',
+                        'data-type': 'invoice_number',
+                    })}>Custom</Button>
                 </ButtonGroup>
 
                 <div className="html-editor__content"

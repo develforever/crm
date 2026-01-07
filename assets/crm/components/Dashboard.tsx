143a302010c0f65f91c1f3f749179de6bf22aecb
Robert Jamróz <rockfade@gmail.com>
2026-01-07 16:27:51 +0100

    some dashboard card style

--- assets/crm/components/Dashboard.tsx
+++ assets/crm/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-
+import './Dashboard.scss';
 
 
 const Dashboard = () => {
@@ -8,9 +8,11 @@ const Dashboard = () => {
         <div>
             <h1>Dashboard</h1>
 
-            <div className='d-flex'>
-                <div className='border'>
-                    <NavLink to="cms" className="link">CMS</NavLink>
+            <div className="flex">
+                <div className='dashboard-card'>
+                    <div className='dashboard-card__wrapper'>
+                        <NavLink to="cms" className="dashboard-card__wrapper__link">CMS</NavLink>
+                    </div>
                 </div>
             </div>
         </div>
